631286b6bf313cc00ec98a03d051482401bbb24c
Srijan Chauhan <srijan.pgp18151@iimkashipur.ac.in>
2026-07-18 02:11:38 +0530

    Make Feel free-text placeholder first-person

--- app/src/PlaylistScreen.js
+++ app/src/PlaylistScreen.js
@@ -963,7 +963,7 @@ export default function PlaylistScreen({ traits }) {
           </View>
           <TextInput
             style={s.moodInput}
-            placeholder="Add more, in your own words (optional)..."
+            placeholder="Add more, in my own words (optional)..."
             placeholderTextColor="#5A5A5A"
             value={extraFeeling}
             onChangeText={setExtraFeeling}
